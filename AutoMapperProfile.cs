73da0464830a461cca5cd19cfd9394157fd29374
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-29 09:01:56 -0300

    feat: add AutoMapper dependency

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using api_rpg.Models;
+using api_rpg.Dtos.Characters;
+
+namespace api_rpg
+{
+	public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+				{
+					CreateMap<Character, GetCharacterDto>();
+					CreateMap<AddCharacterDto, Character>();
+				}
+    }
+}
